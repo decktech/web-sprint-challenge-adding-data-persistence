b5c37e838530c0dc6d4b28141b65550d6de76956
decktech <mdecker113@gmail.com>
2022-08-19 13:40:13 -0400

    wip

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,14 @@
-// build your `/api/projects` router here
+const express = require('express')
+const Recipe = require('./model')
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    Recipe.getRecipeById(req.params.recipe_id)
+      .then(recipe => {
+        res.status(200).json(recipe);
+      })
+      .catch(next); 
+  });
+  
+  module.exports = router
